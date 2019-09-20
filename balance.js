6d83eeed7c90b7dde8cade7013d7fa1e9a6f830b
AKaniru Victory <vic3coorp@gmail.com>
2019-09-20 14:56:49 +0100

    first commit

---
+++ balance.js
@@ -0,0 +1,11 @@
+function processData(input) {
+    //Enter your code here
+    if (input.length % 2 == 0) {
+        console.log(1)
+    }
+
+    if (input.length % 2 != 0) {
+        console.log(0)
+    }
+
+}
---
+++ balanced.py
@@ -0,0 +1,7 @@
+def processData(input):
+    if len(input) % 2 == 0:
+        print(1)
+    elif len(input) % 2 != 0:
+        print(0)
+
+
